75f4312cd2d8cce4672945dab1b264c6d4380fb0
yuraSabadin <yura.sabadin@icloud.com>
2023-08-23 21:14:11 +0300

    ergdfr

--- Objective-C/TOCropViewController/Views/TOCropView.m
+++ Objective-C/TOCropViewController/Views/TOCropView.m
@@ -183,7 +183,7 @@ typedef NS_ENUM(NSInteger, TOCropViewOverlayEdge) {
     //Grey transparent overlay view
     self.overlayView = [[UIView alloc] initWithFrame:self.bounds];
     self.overlayView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    self.overlayView.backgroundColor = [self.backgroundColor colorWithAlphaComponent:0.0f];
+    self.overlayView.backgroundColor = [[UIColor redColor] colorWithAlphaComponent:0.3f];
     self.overlayView.hidden = NO;
     self.overlayView.userInteractionEnabled = NO;
     [self addSubview:self.overlayView];
